79ec6c0f60d2fad4a78c26b380d360e22b797ca6
Alex <ale8under8@gmail.com>
2019-05-27 16:59:58 -0700

    Updates

---
+++ precache-manifest.e35381f48fd81e03e6ae668918888d02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1c7fdd19b32b1a54c00",
+    "url": "/markdown-previewer/static/js/runtime~main.398dfc58.js"
+  },
+  {
+    "revision": "7043176c046d174bc3fd",
+    "url": "/markdown-previewer/static/js/main.bf61153b.chunk.js"
+  },
+  {
+    "revision": "3387e64adff9414458d2",
+    "url": "/markdown-previewer/static/js/2.073a52bf.chunk.js"
+  },
+  {
+    "revision": "7043176c046d174bc3fd",
+    "url": "/markdown-previewer/static/css/main.3544ee19.chunk.css"
+  },
+  {
+    "revision": "ae38f7cfe2d5c13b827379d5e0c57dbc",
+    "url": "/markdown-previewer/index.html"
+  }
+];
